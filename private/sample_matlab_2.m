d89a547f2a55ab4c6214c1b280843b07bc52d253
wangqc0 <w-qc@hotmail.com>
2022-12-16 23:55:53 +0800

    delete unnecessary files

--- private/sample_matlab_2.m
+++
@@ -1,5 +0,0 @@
-% This is a sample file.
-
-proj = openProject('/Users/WilliamWang/Library/Mobile Documents/com~apple~CloudDocs/Documents/Project/chinaMonetaryPolicyDataQuality/ChinaMonetaryPolicyDataQuality.prj');
-
-run_all;
--- private/sample_r_2.R
+++
@@ -1,5 +0,0 @@
-# This is a sample file.
-
-setwd('/Users/WilliamWang/Library/Mobile Documents/com~apple~CloudDocs/Documents/R/gdpntl')
-
-source('main/5_county.R')
